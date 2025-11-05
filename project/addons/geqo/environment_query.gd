0505af6780a60fc887141287cb00e39a8ff48a64
marrero171 <50031007+marrero171@users.noreply.github.com>
2025-11-04 21:06:22 -0400

    Fix paths to say geqo instead

--- project/addons/geqo/environment_query.gd
+++ project/addons/geqo/environment_query.gd
@@ -39,7 +39,7 @@ func request_query() -> QueryResult:
 
 ## Call Debug to draw the items
 func draw_debug(query_items_list: Array[QueryItem]):
-	NQSDebugSpheres.draw_items(query_items_list)
+	GEQODebugSpheres.draw_items(query_items_list)
 
 
 func _notification(what: int) -> void:
