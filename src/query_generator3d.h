167a3290dc42a8fef613a0183ef26c7f0bbe7895
marrero171 <50031007+marrero171@users.noreply.github.com>
2025-11-08 11:25:46 -0400

    Update query_generator3d.h

--- src/query_generator3d.h
+++ src/query_generator3d.h
@@ -1,8 +1,15 @@
 #pragma once
 #include <godot_cpp/classes/node3d.hpp>
+#include "query_result.h"
 
 namespace godot
 {
+    enum RaycastMode
+    {
+        BODY = 0,
+        AREA = 1,
+        BODY_AREA = 2
+    };
     class CQueryGenerator3D : public Node3D
     {
         GDCLASS(CQueryGenerator3D, Node)
@@ -14,7 +21,11 @@ namespace godot
         CQueryGenerator3D();
         ~CQueryGenerator3D();
 
+        void perform_generation(vector<CQueryItem>);
+        void perform_tests(vector<CQueryItem>);
+
     private:
+        RaycastMode raycast_mode = BODY;
     };
 
 }
