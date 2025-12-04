d99b86d08736b35468cd59f48d3cb6546247e30d
marrero171 <50031007+marrero171@users.noreply.github.com>
2025-12-04 16:33:14 -0400

    Removed GDScript version of EnvQuery

--- project/addons/geqo/nodes/contexts/context_target_node.gd
+++
@@ -1,7 +0,0 @@
-extends QueryContext
-class_name ContextTargetNode
-
-@export var target_node: Node3D
-
-func get_context() -> Array[Node3D]:
-    return [target_node]
--- project/addons/geqo/nodes/contexts/context_target_positions.gd
+++
@@ -1,7 +0,0 @@
-extends QueryContext
-class_name ContextTargetPositions
-
-@export var target_positions: Array[Vector3]
-
-func get_context() -> Array[Vector3]:
-    return target_positions
